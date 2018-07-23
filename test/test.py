15c757a53c4e97ab5d9a77a57c1295aee58bbd6c
Matheus Almeida <mat.almeida@live.com>
2018-07-23 12:50:37 -0300

    changing test files location

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ drive_service = service.DriveService('./client_secret.json')
 drive_service.auth()
 
 folder = drive_service.create_folder('Xesque')
-file = drive_service.upload_file('Arquivo Teste', './files/test.pdf', folder)
+file = drive_service.upload_file('Arquivo Teste', './test/files/test.pdf', folder)
 link = drive_service.anyone_permission(file)
 
 print(file)
