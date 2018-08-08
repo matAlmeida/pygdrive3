dfec78b017840a845f4e46ccdba48a7d45d1f9d5
Matheus Almeida <mat.almeida@live.com>
2018-08-08 15:18:12 -0300

    adding get file info

--- pygdrive3/service.py
+++ pygdrive3/service.py
@@ -141,6 +141,13 @@ class DriveService:
 
         return 'https://drive.google.com/file/d/{0}/view?usp=sharing'.format(file_id)
 
+    def get_file_info(self, file_id):
+        fields = "kind,name,mimeType,description,parents,version,createdTime,modifiedTime,lastModifyingUser,size"
+
+        file = self.drive_service.files().get(fileId=file_id, fields=fields).execute()
+
+        return file
+
     def list_files_from_folder_id(self, folder_id):
         itemsList = []
 
