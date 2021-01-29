0d91abe5f57a344d2b53683c0b3b12ceefed77a5
Matheus Almeida <matheus.almeida@natahouse.com>
2021-01-29 15:18:18 -0300

    chore: update version to 0.8.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygdrive3",
-    version="0.7.0",
+    version="0.8.0",
     author="Matheus Almeida",
     author_email="mat.almeida@live.com",
     description="Use Google Drive API v3 with a python interface",
